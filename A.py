25736d45fe8b554e03463fd52bf7d5ab4b569a68
Beatriz Perotto Muniz <77542662+beatrizperottomuniz@users.noreply.github.com>
2026-05-09 20:23:16 -0300

    Refactor main function into top-level code

--- A.py
+++ A.py
@@ -3,23 +3,20 @@ import etcd3
 import time
 import random
 
-def main():
-    client = etcd3.client(host='localhost', port=2379)
 
-    # Lease de 60 segundos: as chaves expiram automaticamente
-    # assim não precisamos limpar manualmente entre execuções
-    lease = client.lease(60)
+client = etcd3.client(host='localhost', port=2379)
 
-    limite = random.randint(10, 20)
+# lease 60 s = as chaves expiram automaticamente
+lease = client.lease(60)
 
-    for i in range(1, limite + 1):
-        print(i)
-        time.sleep(1)
+limite = random.randint(10, 20)
 
-    print("Liberando B e C")
-    client.put('ready/B', '1', lease=lease)
-    client.put('ready/C', '1', lease=lease)
-    print("Fim")
+for i in range(1, limite + 1):
+    print(i)
+    time.sleep(1)
 
-if __name__ == '__main__':
-    main()
+print("Libera B e C")
+client.put('ready/B', '1', lease=lease)
+client.put('ready/C', '1', lease=lease)
+print("Fim")
+
