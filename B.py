61d7aafb4c9584463661745b1a5471cf6adc62ed
Beatriz Perotto Muniz <77542662+beatrizperottomuniz@users.noreply.github.com>
2026-05-09 20:21:42 -0300

    Refactor main function and improve readability

--- B.py
+++ B.py
@@ -3,26 +3,23 @@ import etcd3
 import time
 import random
 
-def main():
-    client = etcd3.client(host='localhost', port=2379)
 
-    print("Aguardando")
+client = etcd3.client(host='localhost', port=2379)
+print("Aguardando")
 
-    # watch_once bloqueia até receber exatamente um evento na chave
-    valor, _ = client.get('ready/B')
-    if valor is None:
-        client.watch_once('ready/B')
+# watch_once bloqueia ate receber exatamente um evento na chave
+valor, _ = client.get('ready/B')
+if valor is None:
+    client.watch_once('ready/B')
 
-    limite = random.randint(10, 20)
+limite = random.randint(10, 20)
 
-    for i in range(1, limite + 1):
-        print(i)
-        time.sleep(1)
+for i in range(1, limite + 1):
+    print(i)
+    time.sleep(1)
 
-    lease = client.lease(60)
-    print("Liberando D")
-    client.put('done/B', '1', lease=lease)
-    print("Fim")
+lease = client.lease(60)
+print("Libera D")
+client.put('done/B', '1', lease=lease)
+print("Fim")
 
-if __name__ == '__main__':
-    main()
